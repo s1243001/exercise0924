d9945e6ab4bc027849c86ed7e9b9ae608d7aeb6b
s1243001 <s1243001@gm.ncue.edu.tw>
2025-09-24 02:15:19 +0000

    modified:   python0924.py find the 3、5、3&5 multiples

--- python0924.py
+++ python0924.py
@@ -14,3 +14,31 @@ student = [
 print (student[1]['name'])  # Accessing Bob's name
 print (student[2]['major'])  # Accessing Charlie's major
 print (student[0]['id'])  # Accessing Alice's id
+
+#寫一個函示，輸入一個數字串列，回傳該數字串列的最大值
+def find_max(numbers):
+    """Return the maximum value from a list of numbers."""
+    if not numbers:
+        raise ValueError("The list is empty")
+    max_value = numbers[0]
+    for num in numbers:
+        if num > max_value:
+            max_value = num
+    return max_value
+print(find_max([3, 1, 4, 1, 5, 9, 2, 6, 5]))  # Example usage
+print("--------------------")
+
+#寫一個程式，從1印到100
+#如果數字是3的倍數，印出"Fizz"
+#如果數字是5的倍數，印出"Buzz"
+#如果數字同時是3和5的倍數，印出"FizzBuzz"
+#其他則印出數字本身
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
