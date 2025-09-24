4ee088445dfc4e9c34f01e4a6a179ea2dd3a4432
s1243001 <s1243001@gm.ncue.edu.tw>
2025-09-24 02:06:19 +0000

    modified:   python0924.py print student

--- python0924.py
+++ python0924.py
@@ -5,3 +5,12 @@ def calculate_circle_area(radius):
         raise ValueError("Radius cannot be negative")
     return math.pi * radius ** 2
 print(calculate_circle_area(5))  # Example usage
+
+student = [
+{'id': 1, 'name': 'Alice', 'major': 'Computer Science'},
+{'id': 2, 'name': 'Bob', 'major': 'Mathematics'},
+{'id': 3, 'name': 'Charlie', 'major': 'Physics'}    
+]
+print (student[1]['name'])  # Accessing Bob's name
+print (student[2]['major'])  # Accessing Charlie's major
+print (student[0]['id'])  # Accessing Alice's id
